032aa4014a578bb9a076901e609c587908ac87e3
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-16 12:31:56 +0100

    Feat: Toggle the ".dropdown-hidden" class off and on

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -11,14 +11,13 @@ class Dropdown {
     this.content = document.querySelector('.dropdown-content');    
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
-      console.log('worked');
+      this.toggleContent();
     })
   }
 
   toggleContent() {
-    
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
