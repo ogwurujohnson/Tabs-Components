54170a2b0bc125331e249b692c89eb50df92f8c7
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-16 13:34:45 +0100

    Feat: Select all tab-item class elements, loop through them, and remove the tabs-item0-selected class which toggles the diesplay between block and hidden

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,27 +13,30 @@ class TabLink {
   
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    console.log(this.tabItem);
-    
     // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', (e) => {
       this.select();
+    })
+  
 
   };
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
+    
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    Array.from(links).forEach(link => {
       link.classList.remove('tabs-link-selected');
+    });
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
     //this.select(this.element);
     const selectedItem = new TabItem(this.itemElement);
-
+    
     selectedItem.select();
   }
 }
@@ -41,17 +44,17 @@ class TabLink {
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element = element;
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
     // const items;
-
+    const items = document.querySelectorAll('.tabs-item');
     // Remove the class "tabs-item-selected" from each element
-    
+    items.forEach(item => item.classList.remove('tabs-item-selected'));
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add('tabs-item-selected');
   }
 }
 
