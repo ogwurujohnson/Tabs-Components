2d028a0e1050f749205508160e2a53e78d426916
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-16 13:31:09 +0100

    Feat: Loop through the links and remove the class making them seem active and  - then add the class showing an active class 'tabs-link-selected'  to the selected element

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -25,7 +25,7 @@ class TabLink {
     // const links;
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+      link.classList.remove('tabs-link-selected');
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
